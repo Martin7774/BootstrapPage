7eccb9251a6416351d06dfc53d0d839ed37545dc
Martin7774 <marcinkon815@gmail.com>
2022-05-29 17:33:12 +0200

    Add new files

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
 	let nav = document.querySelector('.navbar');
+	const link = document.querySelectorAll('.nav-link');
+	const collapse = document.querySelector('.navbar-collapse');
+	//const toggle = document.querySelector('.navbar-toggler');
 
 	function addShadow() {
 		if (window.scrollY >= 150) {
@@ -9,5 +12,21 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	}
 
+	// function addShadowToggle() {
+	// 	if (window.scrollY >= 150) {
+	// 		nav.classList.add('shadow-bg');
+	// 	} else {
+	// 		nav.classList.remove('shadow-bg');
+	// 	}
+	// }
+
 	window.addEventListener('scroll', addShadow);
+
+	link.forEach((item) =>
+		item.addEventListener('click', () => collapse.classList.remove('show'))
+	);
+
+
 });
+
+
